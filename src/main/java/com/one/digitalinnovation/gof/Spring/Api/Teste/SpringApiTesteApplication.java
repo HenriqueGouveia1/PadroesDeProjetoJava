fafbb7f38c4e730aed542ec497ad079d878cad1d
Henrique Gouveia <henriquegs002@gmail.com>
2023-09-30 12:04:51 -0300

    Initial commit

---
+++ src/main/java/com/one/digitalinnovation/gof/Spring/Api/Teste/SpringApiTesteApplication.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.gof.Spring.Api.Teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiTesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/digitalinnovation/gof/Spring/Api/Teste/SpringApiTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.digitalinnovation.gof.Spring.Api.Teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
