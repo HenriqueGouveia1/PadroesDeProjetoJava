f398c475d26fa02f23492f4664b33b275314b9ac
henriquegouveia1 <henriquegs002@gmail.com>
2023-10-02 10:27:11 -0300

    Finalizando testes com Java Puro

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import one.digitalinnovation.gof.SingletonEager;
-import one.digitalinnovation.gof.SingletonLazy;
-import one.digitalinnovation.gof.SingletonLazyHolder;
+import one.digitalinnovation.gof.Facade.Facade;
+import one.digitalinnovation.gof.Singleton.SingletonEager;
+import one.digitalinnovation.gof.Singleton.SingletonLazy;
+import one.digitalinnovation.gof.Singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.Strategy.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testando e Checando os endereços de memória relacionado ao Design Patter do Singleton.");
+
+
+        System.out.println("Testando e Checando os endereços de memória relacionado ao Design Patter do Singleton: ");
 
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
@@ -20,5 +24,27 @@ public class Main {
         System.out.println(lazyH);
         lazyH = SingletonLazyHolder.getInstancia();
         System.out.println(lazyH);
+
+        System.out.println();
+        System.out.println("Testando o Strategy:");
+        Comportamento d = new ComportamentoDefensivo();
+        Comportamento n = new ComportamentoNormal();
+        Comportamento a = new ComportamentoAgressivo();
+
+        Robo r = new Robo();
+        r.setStrategy(n);
+        r.mover();
+
+        r.setStrategy(d);
+        r.mover();
+
+        r.setStrategy(a);
+        r.mover();
+
+        System.out.println("Testando o Facade:");
+        Facade f = new Facade();
+        f.migrarCliente("nome","123123");
+
+
     }
 }
